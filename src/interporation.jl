d91249bed0c716b9109ef1163a8fdeec10136b21
MIKANkankitsu <kankitsumikan0424@gmail.com>
2026-02-22 11:02:46 +0900

    fix bug

--- src/interporation.jl
+++ src/interporation.jl
@@ -17,17 +17,17 @@ The shift operator is defined on the admissible Hoffman algebra (words of 0 and 
 For `Index` elements, they are converted to `Hoffman` elements based on the `orientation`, multiplied, and converted back.
 """
 
-function monomial_shift(w::HoffmanWord; t=0//1::Rational{BigInt})
+function monomial_shift(w::HoffmanWord; t=big(0)//1::Rational{BigInt})
     k = HoffmanWord(w.t[2:end])
     return y*Hoffman_hom(k, [x, t*x + y])
 end
 
-function monomial_shift(w::IndexWord; t=0//1::Rational{BigInt})
+function monomial_shift(w::IndexWord; t=big(0)//1::Rational{BigInt})
     k = HoffmanWord(HoffmanWord(w).t[2:end])
     return Index(y*Hoffman_hom(k, [x, t*x + y]))
 end
 
-function shift(w::Hoffman; t=0//1::Rational{BigInt})
+function shift(w::Hoffman; t=big(0)//1::Rational{BigInt})
     ans = zero(Hoffman)
     for (h, c) in w.terms
         hd = c*monomial_shift(h; t=t)
@@ -36,9 +36,9 @@ function shift(w::Hoffman; t=0//1::Rational{BigInt})
     return ans
 end
 
-shift(w::HoffmanWord; t = 0//1::Rational{BigInt}) = monomial_shift(w; t=t)
-shift(idx::Index; t=0//1::Rational{BigInt}) = Index(shift(Hoffman(idx); t=t))
-shift(idx::IndexWord; t=0//1::Rational{BigInt}) = monomial_shift(idx; t=t)
+shift(w::HoffmanWord; t = big(0)//1::Rational{BigInt}) = monomial_shift(w; t=t)
+shift(idx::Index; t=big(0)//1::Rational{BigInt}) = Index(shift(Hoffman(idx); t=t))
+shift(idx::IndexWord; t=big(0)//1::Rational{BigInt}) = monomial_shift(idx; t=t)
 
 """
     sharp_shift(w::Hoffman)
@@ -48,7 +48,7 @@ Computes "sharp-MZV" such that 2^weight * shift(;t=1/2).
 The shift operator is defined on the admissible Hoffman algebra (words of 0 and 1).
 For `Index` elements, they are converted to `Hoffman` elements based on the `orientation`, multiplied, and converted back.
 
-sharp_shifted Indexes are important e.g. Zhao's 2-1 formula.
+sharp_shifted indexes are important e.g. Zhao's 2-1 formula.
 """
 
 monomial_sharp_shift(kk::HoffmanWord) = 2^sum(kk.t)*shift(kk; t=big(1)//2)
